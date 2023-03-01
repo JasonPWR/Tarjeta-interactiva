6734fcb75fa63adf3a0232361027b5b4ea868870
Jeison Primo <jvtprimo@gmail.com>
2023-03-01 00:26:08 -0400

    interactividad con js y validacion de numeros en el input

--- index.js
+++ index.js
@@ -1,36 +1,83 @@
-const cardNumberInput = document.querySelector(".card-number-input");
-const cardNumber = document.querySelector(".card-number");
-const cardName = document.querySelector(".card-name");
-const cardNameInput = document.querySelector(".card-name-input");
+// CardNumber selector 
+const numberCardInput = document.querySelector(".card-number-input");
+const numberCard = document.querySelector(".card-number");
+
+// cardName Selector 
+const nameCard = document.querySelector("#card-name");
+const nameCardInput = document.querySelector(".card-name-input");
+
+// Date selector 
 const monthCard = document.querySelector(".month-card");
 const yearCard = document.querySelector(".year-card");
 const mm = document.querySelector(".m-input");
 const yy = document.querySelector(".y-input");
+
+// CVC Selector 
 const cvcInput = document.querySelector(".cvc-input");
 const cvcCard = document.querySelector(".cvc-card");
 
+// Error selector
+const error = document.querySelector("#error");
 
-cardNumberInput.addEventListener("keyup", (e) => {
-    let cardN = e.target.value.match(/.{1,4}/gi);
-    // carN create a array
-        cardNumber.textContent = cardN.join(" ");
-})
-
-cardNameInput.addEventListener("keyup", (e) => {
-    cardName.textContent = e.target.value;
+//  Change Number card
+numberCardInput.addEventListener("input", (e) => {
+    // Chaging number card interactively
+    let inputValue = e.target.value;
+    numberCard.textContent = numberCardInput.value;
     
-})
+    // validando letras
+    const isThereAnyLetter = /[A-z]/g;
+    if(isThereAnyLetter.test(numberCardInput.value)){
 
-mm.addEventListener("keyup", (e) => {
-    monthCard.textContent = e.target.value;
-})
-yy.addEventListener("keyup", (e) => {
-    yearCard.textContent = e.target.value;
-})
+        showError(error,numberCardInput,"ala bestia")
+    }
+    else{  //añadiendo espacios cada 4 numeros y añadiendolos automaticamente
+        numberCardInput.value = inputValue.replace(/\s/g, "").replace(/([0-9]{4})/g,"$1 ").trim();
+        // eliminando error
+        hideError(error,numberCardInput);
 
-cvcInput.addEventListener("keyup", (e) => {
-    cvcCard.textContent = e.target.value;
+    }
+    if(numberCardInput.value == ""){
+        numberCard.textContent = "0000 0000 0000 0000"; 
+    }
 });
-// const setDateCard = (mm,yy) => {
 
-// }
+// change name card
+nameCardInput.addEventListener("input", () => {
+    const isInputEmpty = nameCardInput.value;
+    if(isInputEmpty == ''){
+        nameCard.textContent = "Your Name";
+    }else{
+        nameCard.textContent = nameCardInput.value;
+    }
+
+});
+
+mm.addEventListener("input", (e) => {
+    const inputValue = e.target.value;
+
+    monthCard.textContent = inputValue;
+});
+yy.addEventListener("input", (e) => {
+    const inputValue = e.target.value;
+
+    yearCard.textContent = inputValue;
+});
+
+cvcInput.addEventListener("input", (e) => {
+    const inputValue = e.target.value;
+
+    cvcCard.textContent = inputValue;
+});
+
+function showError(pError,inputError,msgError){
+    pError.classList.add("error");
+    pError.textContent = msgError;
+    inputError.style.borderColor = "#ff5252"
+    
+}
+function hideError(pError,inputError){
+    pError.classList.remove("error");
+    pError.textContent = "";
+    inputError.style.borderColor = "#0000001f"
+}
